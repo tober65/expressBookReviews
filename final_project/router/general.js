9c01d28ccb6cef501bd485b983137b7530eea609
Timothy Ober <timothy.ober@gmail.com>
2024-04-16 18:56:45 -0400

    Redid tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,7 +2,6 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
-const axios = require('axios');
 const public_users = express.Router();
 
 
@@ -36,11 +35,26 @@ public_users.get('/',function (req, res) {
   return res.status(200).send(JSON.stringify(books, null, " "));
 });
 
+//Task 10
+public_users.get('/async-books', async function (req, res) {
+    await new Promise((resolve, reject)=>{
+        resolve(res.status(200).send(JSON.stringify(books, null, " ")));
+    })
+  });
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   return res.status(200).send(JSON.stringify(books[req.params.isbn], null, " "));
  });
+
+ //Task 11
+ public_users.get('/async-isbn/:isbn', async function (req, res) {
+    //Write your code here
+    await new Promise((resolve, reject)=>{
+        resolve(res.status(200).send(JSON.stringify(books[req.params.isbn], null, " ")));
+    })
+   });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -54,6 +68,20 @@ public_users.get('/author/:author',function (req, res) {
   return res.status(200).send(JSON.stringify(matchingBooks, null, " "));
 });
 
+//Task 12
+public_users.get('/async-author/:author', async function (req, res) {
+    //Write your code here
+    let matchingBooks = [];
+    Object.keys(books).forEach(key => {
+      if (books[key].author == req.params.author) {
+          matchingBooks.push(books[key]);
+      }
+    });
+    await new Promise((resolve, reject)=>{
+        resolve(res.status(200).send(JSON.stringify(matchingBooks, null, " ")));
+    })
+  });
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
@@ -66,22 +94,24 @@ public_users.get('/title/:title',function (req, res) {
   return res.status(200).send(JSON.stringify(matchingBooks, null, " "));
 });
 
+//Task 13
+public_users.get('/async-title/:title', async function (req, res) {
+    //Write your code here
+    let matchingBooks = [];
+    Object.keys(books).forEach(key => {
+      if (books[key].title == req.params.title) {
+          matchingBooks.push(books[key]);
+      }
+    });
+    await new Promise((resolve, reject)=>{
+        resolve(res.status(200).send(JSON.stringify(matchingBooks, null, " ")));
+    })
+  });
+
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   return res.status(200).send(JSON.stringify(books[req.params.isbn].reviews, null, " "));
 });
-
-const reqAllBooks = axios.get("https://timothyober-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/");
-reqAllBooks.then(resp => console.log(resp.data));
-
-const reqByIsbn = axios.get("https://timothyober-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn/1");
-reqByIsbn.then(resp => console.log(resp.data));
-
-const reqByAuthor = axios.get("https://timothyober-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author/Jane Austen");
-reqByAuthor.then(resp => console.log(resp.data));
-
-const reqByTitle = axios.get("https://timothyober-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title/Things Fall Apart");
-reqByTitle.then(resp => console.log(resp.data));
 
 module.exports.general = public_users;
