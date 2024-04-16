819d8bc8a4e8a4b4c05214da6575fb87fa34672f
Timothy Ober <timothy.ober@gmail.com>
2024-04-15 20:15:20 -0400

    Implmented up to task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,25 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books, null, " "));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books[req.params.isbn], null, " "));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let matchingBooks = [];
+  Object.keys(books).forEach(key => {
+    if (books[key].author == req.params.author) {
+        matchingBooks.push(books[key]);
+    }
+  });
+  return res.status(200).send(JSON.stringify(matchingBooks, null, " "));
 });
 
 // Get all books based on title
